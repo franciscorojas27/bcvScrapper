4e2a42d6cacd1ce5a405916e6e95e4757a7843e8
Francisco Rojas <franciscoantonior30@gmail.com>
2026-04-30 11:06:57 -0400

    Migrar servidor a Fiber y reorganizar inicialización; actualizar dependencias (go.mod/go.sum); añadir docker-compose.yml
    Incluye:
    - main.go: usar Fiber, reorganizar init, manejar DB en App
    - go.mod / go.sum: actualizar y añadir dependencias
    - docker-compose.yml: añadir servicio Postgres para desarrollo

--- main.go
+++ main.go
@@ -1,14 +1,12 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log/slog"
-	"net/http"
 	"os"
 
+	"github.com/gofiber/fiber/v3"
 	"github.com/jackc/pgx/v5/pgxpool"
-	"github.com/rs/cors"
 )
 
 type App struct {
@@ -22,6 +20,7 @@ var (
 	chatIDTelegram = os.Getenv("TELEGRAM_CHAT_ID")
 	FinalData      CurrencyRatesData
 	logger         = slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	app            *App
 )
 
 func init() {
@@ -36,27 +35,27 @@ func init() {
 		logger.Warn("the environment variables TELEGRAM_TOKEN o TELEGRAM_CHAT_ID not set.")
 		panic(0)
 	}
-}
 
-func main() {
-	FinalData = scrapeBCV()
 	pool, err := connectDB(conString)
 	if err != nil {
 		logger.Error("Error to connect to database", "error", err)
 		return
 	}
+	defer pool.Close()
+
 	if err = initializeDB(pool); err != nil {
 		logger.Error("Error to initialize the database", "error", err)
 		return
 	}
-	if err = SaveScrapeReport(pool, FinalData); err != nil {
+	app = &App{DB: pool}
+}
+
+func main() {
+	FinalData = scrapeBCV()
+	if err := SaveScrapeReport(app.DB, FinalData); err != nil {
 		logger.Error("Error to save scrape report", "error", err)
 		return
 	}
-
-	defer pool.Close()
-
-	// app := &App{DB: pool}
 
 	// authTelegram := AuthTelegram{
 	// 	Token:  tokenTelegram,
@@ -71,18 +70,17 @@ func main() {
 	// 	logger.Info("Message sent to Telegram successfully")
 	// }
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("/rates", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		date, err := GetLatestRates(pool)
-		if err != nil {
-			http.Error(w, fmt.Sprintf("Error fetching latest rates: %v", err), http.StatusInternalServerError)
-			return
-		}
-		json.NewEncoder(w).Encode(date)
-	})
+	server := fiber.New()
 
-	handler := cors.Default().Handler(mux)
-	fmt.Printf("Server http://localhost:%s\n", port)
-	http.ListenAndServe(":"+port, handler)
+	server.Get("/rates", func(c fiber.Ctx) error {
+		date, err := GetLatestRates(app.DB)
+		logger.Info("Request: /rates", "ip:", c.IP())
+		if err != nil {
+			logger.Error("Error fetching latest rates", "error", err)
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": fmt.Sprintf("Error fetching latest rates: %v", err)})
+		}
+		return c.JSON(date)
+	})
+	logger.Info("Starting server", "port", port)
+	server.Listen(":" + port)
 }
